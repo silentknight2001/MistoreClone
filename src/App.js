26aeef10ecbeaa515bc40bd11cf759a8da75ed2f
silentknight2001 <silentknight.www@gmail.com>
2022-05-17 20:35:31 +0530

    MiStore

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PreNavbar from "./Components/PreNavbar";
+import Navbar from "./Components/Navbar.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Slider from "./Components/Slider.js";
+import Offers from "./Components/Offers.js";
+import data from "./data/data.json";
+import Heading from "./Components/Heading.js";
+import StarProduct from "./Components/StarProduct.js";
+import HotAccessoriesManu from "./Components/HotAccessoriesManu.js";
+import HotAccessories from "./components/HotAccessories.js";
+import ProductReviews from "./Components/ProductReviews.js"
+import Videos from "./Components/Videos.js"
+import Banner from "./Components/Banner.js"
+import Footer from "./Components/Footer.js"
+import NavOptios from "./Components/NavOptions.js"
+
+
+
+// const {banner} = data; // it's created for banner but after includ directly{data.banner.start} its now useless.............................................................................
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <PreNavbar />
+      <Navbar />
+      
+      <NavOptios miPhones={data.miPhones} redmiPhones={data.redmiPhones} tv={data.tv} laptop={data.laptop} fitnessAndLifeStyle={data.fitnessAndLifeStyle} home={data.home} audio={data.audio}  accessories={data.accessories}/>
+
+
+      <Slider start={data.banner.start} />
+      <Offers offer={data.offer} />
+      <Heading text="STAR PRODUCTS" />
+      <StarProduct starProduct={data.starProduct} />
+      <Heading text="HOT ACCESSORIES" />
+      <HotAccessoriesManu />
+
+      <Routes>
+        <Route
+          exact
+          path="/music"
+          element={
+            <HotAccessories
+              music={data.hotAccessories.music}
+              musicCover={data.hotAccessoriesCover.music}
+            />
+          }
+        />
+        <Route />
+
+        <Route
+          exact
+          path="/smartDevice"
+          element={
+            <HotAccessories
+            smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice}
+            />
+          }
+        />
+        <Route />
+
+        <Route exact path="/home" element={ <HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home} /> }/>
+        <Route />
+
+        <Route exact path="/lifestyle" element={ <HotAccessories lifeStyle={data.hotAccessories.lifeStyle} lifeStyleCover={data.hotAccessoriesCover.lifeStyle} /> }/>
+        <Route />
+
+        <Route exact path="/mobileAccessories" element={ <HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories} /> }/>
+        <Route />
+      </Routes>
+      <Heading text="PRODUCT REVIEWS"/>
+      <ProductReviews productReviews={data.productReviews}/>
+      <Heading text = "VIDEOS" />
+
+      <Videos videos={data.videos}/>
+
+      <Heading text="IN THE PRESS"/>
+      <Banner banner={data.banner}/>
+      <Footer footer={data.footer}/>
+      
+
+     
+    </Router>
   );
 }
 
---
+++ src/Components/Footer.js
@@ -0,0 +1,115 @@
+import React from 'react'
+import "../Styles/preFooter.css"
+import "../Styles/Footer.css"
+
+
+
+
+const repeat = <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 6v3l4-4-4-4v3c-4.42 0-8 3.58-8 8 0 1.57.46 3.03 1.24 4.26L6.7 14.8c-.45-.83-.7-1.79-.7-2.8 0-3.31 2.69-6 6-6zm6.76 1.74L17.3 9.2c.44.84.7 1.79.7 2.8 0 3.31-2.69 6-6 6v-3l-4 4 4 4v-3c4.42 0 8-3.58 8-8 0-1.57-.46-3.03-1.24-4.26z"/></svg>
+
+const shield = <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm7 10c0 4.52-2.98 8.69-7 9.93-4.02-1.24-7-5.41-7-9.93V6.3l7-3.11 7 3.11V11zm-11.59.59L6 13l4 4 8-8-1.41-1.42L10 14.17z" /></svg>
+
+const map = <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20.5 3l-.16.03L15 5.1 9 3 3.36 4.9c-.21.07-.36.25-.36.48V20.5c0 .28.22.5.5.5l.16-.03L9 18.9l6 2.1 5.64-1.9c.21-.07.36-.25.36-.48V3.5c0-.28-.22-.5-.5-.5zM10 5.47l4 1.4v11.66l-4-1.4V5.47zm-5 .99l3-1.01v11.7l-3 1.16V6.46zm14 11.08l-3 1.01V6.86l3-1.16v11.84z"/></svg>
+
+const faceIcon = <svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px">    <path d="M15,3C8.373,3,3,8.373,3,15c0,6.016,4.432,10.984,10.206,11.852V18.18h-2.969v-3.154h2.969v-2.099c0-3.475,1.693-5,4.581-5 c1.383,0,2.115,0.103,2.461,0.149v2.753h-1.97c-1.226,0-1.654,1.163-1.654,2.473v1.724h3.593L19.73,18.18h-3.106v8.697 C22.481,26.083,27,21.075,27,15C27,8.373,21.627,3,15,3z"/></svg>
+
+const twiterIcon = <svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px">    <path d="M28,6.937c-0.957,0.425-1.985,0.711-3.064,0.84c1.102-0.66,1.947-1.705,2.345-2.951c-1.03,0.611-2.172,1.055-3.388,1.295 c-0.973-1.037-2.359-1.685-3.893-1.685c-2.946,0-5.334,2.389-5.334,5.334c0,0.418,0.048,0.826,0.138,1.215 c-4.433-0.222-8.363-2.346-10.995-5.574C3.351,6.199,3.088,7.115,3.088,8.094c0,1.85,0.941,3.483,2.372,4.439 c-0.874-0.028-1.697-0.268-2.416-0.667c0,0.023,0,0.044,0,0.067c0,2.585,1.838,4.741,4.279,5.23 c-0.447,0.122-0.919,0.187-1.406,0.187c-0.343,0-0.678-0.034-1.003-0.095c0.679,2.119,2.649,3.662,4.983,3.705 c-1.825,1.431-4.125,2.284-6.625,2.284c-0.43,0-0.855-0.025-1.273-0.075c2.361,1.513,5.164,2.396,8.177,2.396 c9.812,0,15.176-8.128,15.176-15.177c0-0.231-0.005-0.461-0.015-0.69C26.38,8.945,27.285,8.006,28,6.937z"/></svg>
+
+const instaIcon = <svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 32 32" width="32px" height="32px"><path d="M 11.46875 5 C 7.917969 5 5 7.914063 5 11.46875 L 5 20.53125 C 5 24.082031 7.914063 27 11.46875 27 L 20.53125 27 C 24.082031 27 27 24.085938 27 20.53125 L 27 11.46875 C 27 7.917969 24.085938 5 20.53125 5 Z M 11.46875 7 L 20.53125 7 C 23.003906 7 25 8.996094 25 11.46875 L 25 20.53125 C 25 23.003906 23.003906 25 20.53125 25 L 11.46875 25 C 8.996094 25 7 23.003906 7 20.53125 L 7 11.46875 C 7 8.996094 8.996094 7 11.46875 7 Z M 21.90625 9.1875 C 21.402344 9.1875 21 9.589844 21 10.09375 C 21 10.597656 21.402344 11 21.90625 11 C 22.410156 11 22.8125 10.597656 22.8125 10.09375 C 22.8125 9.589844 22.410156 9.1875 21.90625 9.1875 Z M 16 10 C 12.699219 10 10 12.699219 10 16 C 10 19.300781 12.699219 22 16 22 C 19.300781 22 22 19.300781 22 16 C 22 12.699219 19.300781 10 16 10 Z M 16 12 C 18.222656 12 20 13.777344 20 16 C 20 18.222656 18.222656 20 16 20 C 13.777344 20 12 18.222656 12 16 C 12 13.777344 13.777344 12 16 12 Z"/></svg>
+
+const youtubIcon = <svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px">    <path d="M21.582,6.186c-0.23-0.86-0.908-1.538-1.768-1.768C18.254,4,12,4,12,4S5.746,4,4.186,4.418 c-0.86,0.23-1.538,0.908-1.768,1.768C2,7.746,2,12,2,12s0,4.254,0.418,5.814c0.23,0.86,0.908,1.538,1.768,1.768 C5.746,20,12,20,12,20s6.254,0,7.814-0.418c0.861-0.23,1.538-0.908,1.768-1.768C22,16.254,22,12,22,12S22,7.746,21.582,6.186z M10,14.598V9.402c0-0.385,0.417-0.625,0.75-0.433l4.5,2.598c0.333,0.192,0.333,0.674,0,0.866l-4.5,2.598 C10.417,15.224,10,14.983,10,14.598z"/></svg>
+
+
+const Footer = ({footer}) => {
+  return (
+      <> 
+    <div className='preFooter'>
+        <div>
+            {repeat}
+            <p><b>Hassle-free replacement</b> <br/>10-day easy replacement policy on mi.com</p>
+        </div>
+        <div>
+            {shield}
+            <p><b>100% secure payment</b> <br/>we support Card, Asllets, EMI and COD</p>
+        </div>
+        <div>
+            { map}
+            <p><b>Vast service network</b> <br/> 1000 mi service-center across 600 cities</p>
+        </div>
+    </div>
+        <div className="preFooter2">
+            <div> <p>LET,S STAY IN TOUCH</p><span>Get updates on sales specials and more</span></div>
+            <div>
+                <div>
+                <input type="Email" name="Email" placeholder='Enter Email Address' />
+                <button>›</button>
+                </div>
+                <span>Thanks.you're on our email list for special offer.</span>
+            </div>
+            <div>
+                <p>FOLLOW MI</p>
+                <span>We want to here from you!</span>
+            </div>
+            <div>
+                {faceIcon} {youtubIcon} {instaIcon} {twiterIcon}
+            </div>
+        </div>
+
+        <div className="Footer">
+            <div>
+                <p>SUPPORT</p>
+                {footer.support.map((item,index)=>(
+                    <a key={item.url} href={item.url} > {item.name}</a>
+
+                ))}
+            </div>
+
+            <div>
+                <p>SHOPE AND LEARN</p>
+                {footer.shopAndLearn.map((item,index)=>(
+                    <a key={item.url}href={item.url}>{item.name}</a>
+
+                ))}
+            </div>
+            <div>
+                <p>RETAIL STORE</p>
+
+                {footer.retailStore.map((item,index)=>(
+                    <a key={item.url}href={item.url}>{item.name}</a>
+                ))}
+            </div>
+
+            <div>
+                <p>ABOUT</p>
+                {footer.aboutUS.map((item,index)=>(
+                    <a key={item.url} href={item.url}>{item.name}</a>
+                ))}
+            </div>
+
+            <div>
+                <p>CONTACT US</p>
+                {footer.contactUs.map((item,index)=>(
+                    <a key={item.url} href={item.url}>{item.name}</a>
+                ))}
+            </div>
+
+            <div className='chat'>
+                <p>Chat With Our Virtual AI Bot (24/7 Live Support) </p>
+                <button>Chat Now</button>
+            </div>
+
+
+
+
+        </div>
+            <div className='footerBorder'>
+
+                <div>Copyright © 2010 - 2025 Xiaomi. All Right Reserve</div>
+
+            </div>
+
+    </>
+  )
+}
+
+export default Footer
